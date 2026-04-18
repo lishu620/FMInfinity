7a3e703596ed4731de51f3ae518d725cb0494725
lishu620 <mlishu@163.com>
2026-04-18 19:08:38 +0800

    删除 dist/assets 中不再需要的文件

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 
 // 前端（非调试）
-// app.use(express.static(__dirname + "/dist"));
+app.use(express.static(path.join(__dirname, "dist")));
 
 // 中间件
 app.use(cors());
